fade620ffe40da7da488cfbcc3f7a06d246ed092
Harshil Patel <46099278+harshilpatel95@users.noreply.github.com>
2020-07-07 11:46:47 -0700

    Delete Main.ml

--- Project 2/Interpreter/Main.ml
+++
@@ -1,7 +0,0 @@
-(* Main program:  runs our tests *)
-
-
-(* switch EvalEnv to EvalSubst to test substitution-based interpreter *)
-module Eval = EvalEnv
-
-let main = Testing.run_tests Eval.debug_eval Testing.tests
