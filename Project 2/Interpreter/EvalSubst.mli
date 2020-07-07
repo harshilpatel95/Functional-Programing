eca59836f8d6c5dd1b456bc079737c0da4038551
Harshil Patel <46099278+harshilpatel95@users.noreply.github.com>
2020-07-07 11:45:09 -0700

    Delete EvalSubst.mli

--- Project 2/Interpreter/EvalSubst.mli
+++
@@ -1,9 +0,0 @@
-(*************************************************)
-(* A substitution-based evaluator for Dynamic ML *)
-(*************************************************)
-
-(* evaluate input to a value *)
-val eval : Syntax.exp -> Syntax.exp
-
-(* evaluate input to a value while printing intermediate results *)
-val debug_eval : Syntax.exp -> Syntax.exp
